c7feb620393cb464dc472b327e13dec8a40e33e7
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-09-25 19:12:59 -0400

    figured out prolog list formatting

--- Assign2-AJF.pl
+++ Assign2-AJF.pl
@@ -2,11 +2,12 @@
 
 % find length of list.
 myLength([], 0).
-myLength([X|Rest], Len) :- myLength(Rest, X), Len is +(1, X).
+myLength([_|Rest], Len) :- myLength(Rest, X), Len is +(1, X).
 
 % find last entry in list.
-myLast([L], L).
-myLast([Head|Rest], L) :- myLast(Rest,L).
+myLast([Last], Last).
+myLast([_|Rest], Last) :- myLast(Rest, Last).
 
 % find max val in list.
 % run by typing swipl prolog.pro.
+% commands are run by typing myLength([1,2,3],X).
