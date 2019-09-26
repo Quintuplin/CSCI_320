040a8898486d4a503f0924f34dda736b3b6b482e
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-09-25 21:23:02 -0400

    formatting and myRemove (v1)

--- Assign2-AJF.pl
+++ Assign2-AJF.pl
@@ -4,37 +4,53 @@
 % 9/26/2019.
 
 
-% myLength(List, Len).
-% find length of list (done).
-myLength([], 0).
-myLength([_|Rest], Len) :- myLength(Rest, X), Len is +(1, X).
-
-
-% myLast(List, L).
-% find last entry in list (done).
-myLast([L], L).
-myLast([_|Rest], L) :- myLast(Rest, L).
-
-
-% myMax(List, MaxVal).
-% find max val in list (incomplete; works for ascending values, but not for descending).
-% utilizes append predicate which is not the subject of any of the questions in this assignment; as such by the wording of the questions it should be valid.
-
-% base condition and submethods.
-myMax([MaxVal], MaxVal).
-myTop([], 0).
-myTop([Head|_], Head).
-myRest([Head], []).
-myRest([_|Rest], Rest).
-
-% condition for ascending segment.
-myMax([Head|Rest], MaxVal) :- myTop(Rest, X), X >= Head, myMax(Rest, MaxVal).
-
-% condition for descending segment.
-myMax([Head|Rest], MaxVal) :- myTop(Rest, X), X < Head, myRest(Rest, Y), append([Head], Y, HeadY), myMax(HeadY, MaxVal).
-
-% remove val from list.
-myRemove()
-
 % run by typing swipl prolog.pro.
 % commands are run by typing myLength([1,2,3],X).
+
+
+% 1. myLength(List, Len).
+    % find length of list (done).
+
+    myLength([], 0).
+    myLength([_|Rest], Len) :- myLength(Rest, X), Len is +(1, X).
+
+
+% 2. myLast(List, L).
+    % find last entry in list (done).
+
+    myLast([L], L).
+    myLast([_|Rest], L) :- myLast(Rest, L).
+
+
+% 3. myMax(List, MaxVal).
+    % find max val in list.
+
+    % submethods.
+    % myTop is for double-dereferencing head within a single main loop.
+    myTop([], 0).
+    myTop([Head|_], Head).
+    % myRest is for double-dereferencing rest within a single main loop.
+    myRest([_], []).
+    myRest([_|Rest], Rest).
+
+    % base condition.
+    % the last item left in the list is the largest.
+    myMax([MaxVal], MaxVal).
+
+    % condition for ascending segment.
+    % removes head and repeats.
+    myMax([Head|Rest], MaxVal) :- myTop(Rest, X), X >= Head, myMax(Rest, MaxVal).
+
+    % condition for descending segment.
+    % removes next head, puts current head back in, then repeats.
+    myMax([Head|Rest], MaxVal) :- myTop(Rest, X), X < Head, myRest(Rest, Y), HeadZ = [Head|Y], myMax(HeadZ, MaxVal).
+
+
+% 4. myRemove(X, List, Redlist).
+    % remove x from list, return as Redlist.
+
+    % base condition.
+    myRemove(_, [], _).
+
+    myRemove(X, [Head|Rest], Redlist) :- X != Head, myRemove(X, Rest, [Redlist|Head]).
+    myRemove(X, [Head|Rest], Redlist) :- X == Head, myRemove(X, Rest, Redlist).
