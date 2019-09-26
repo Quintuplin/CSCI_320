bf03d5c91bc7d3c54255b8c1294dc74585027fb6
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-09-25 21:45:31 -0400

    myRemove done

--- Assign2-AJF.pl
+++ Assign2-AJF.pl
@@ -12,14 +12,14 @@
     % find length of list (done).
 
     myLength([], 0).
-    myLength([_|Rest], Len) :- myLength(Rest, X), Len is +(1, X).
+    myLength([_|Tail], Len) :- myLength(Tail, X), Len is +(1, X).
 
 
 % 2. myLast(List, L).
     % find last entry in list (done).
 
     myLast([L], L).
-    myLast([_|Rest], L) :- myLast(Rest, L).
+    myLast([_|Tail], L) :- myLast(Tail, L).
 
 
 % 3. myMax(List, MaxVal).
@@ -29,9 +29,9 @@
     % myTop is for double-dereferencing head within a single main loop.
     myTop([], 0).
     myTop([Head|_], Head).
-    % myRest is for double-dereferencing rest within a single main loop.
+    % myRest is for double-dereferencing tail within a single main loop.
     myRest([_], []).
-    myRest([_|Rest], Rest).
+    myRest([_|Tail], Tail).
 
     % base condition.
     % the last item left in the list is the largest.
@@ -39,18 +39,18 @@
 
     % condition for ascending segment.
     % removes head and repeats.
-    myMax([Head|Rest], MaxVal) :- myTop(Rest, X), X >= Head, myMax(Rest, MaxVal).
+    myMax([Head|Tail], MaxVal) :- myTop(Tail, X), X >= Head, myMax(Tail, MaxVal).
 
     % condition for descending segment.
     % removes next head, puts current head back in, then repeats.
-    myMax([Head|Rest], MaxVal) :- myTop(Rest, X), X < Head, myRest(Rest, Y), HeadZ = [Head|Y], myMax(HeadZ, MaxVal).
+    myMax([Head|Tail], MaxVal) :- myTop(Tail, X), X < Head, myRest(Tail, Y), myMax([Head|Y], MaxVal).
 
 
 % 4. myRemove(X, List, Redlist).
     % remove x from list, return as Redlist.
 
     % base condition.
-    myRemove(_, [], _).
+    myRemove(_, [], []).
 
-    myRemove(X, [Head|Rest], Redlist) :- X != Head, myRemove(X, Rest, [Redlist|Head]).
-    myRemove(X, [Head|Rest], Redlist) :- X == Head, myRemove(X, Rest, Redlist).
+    myRemove(X, [Head|Tail], Redlist) :- X \= Head, Redlist = [Head|Redtail], myRemove(X, Tail, Redtail).
+    myRemove(X, [Head|Tail], Redlist) :- X = Head, myRemove(X, Tail, Redlist).
