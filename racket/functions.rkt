6b36a8b8855cf8cd2853622d445a5166f7b0d862
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-10-10 15:04:06 -0400

    new functions and more wip

--- racket/functions.rkt
+++ racket/functions.rkt
@@ -2,14 +2,62 @@
 ; Run with the command 'racket racket/functions.rkt'
 
 ;member? - returns #t if atom is in a list, #f otherwise
-(define (member? atom list)(cond
-    ((null? list) #f)
-    ((eqv? (car(list)) atom) #t)
-    (else (member? atom cdr(list)) #t)
+(define (member? atom lst)(cond
+    ((null? lst) #f)
+    ((eqv? (car lst) atom) #t)
+    (else (member? atom (cdr lst)))
 ))
 
-(member 1 '(1 2 3))
-(member 1 '(2 1 3))
-(member 1 '(2 1))
-(member 1 '(1))
-(member 1 '(2))
+'(test member)
+(member? 1 '(1 2 3))
+(member? 1 '(2 1 3))
+(member? 1 '(2 1))
+(member? 1 '(1))
+(member? 1 '(2))
+(member? 1 '(2 2 3))
+
+;append - takes two lists, returns list 1 then list 2
+(define (append list1 list2)(cond
+    ((null? list1) list2)
+    (else (cons (car list1) (append (cdr list1) list2)))
+))
+
+'(test append)
+(append '() '(1))
+(append '(1) '())
+(append '(1 2 3) '(4 5 6))
+
+;reverse - takes list, returns reversed list
+(define (reverse lst)(cond
+    ((null? lst) lst)
+    (else (append (reverse (cdr lst)) (list(car lst))))
+))
+
+'(test reverse)
+(reverse '(1 2 3))
+(reverse '(4 5 6))
+(reverse '())
+
+;sqr
+
+;apply_to_all - applies a single-value function to every value within a list, returns list
+(define (apply_to_all func lst)(cond
+    ((null? lst) lst)
+    (else (cons (func (car lst) (apply_to_all func (cdr lst)))))
+))
+
+'(test apply_to_all)
+(apply_to_all sqr '(1 2 3))
+
+;make_list_size - takes number n, list e; creates a list containing n copies of e
+(define (make_list_size n e)(cond
+    ((<= n 0) '())
+    ((null? e) '())
+    (else(append((list e) (make_list_size (- n 1) e))))
+))
+
+'(test make_list_size)
+(make_list_size 2 4)
+(make_list_size 4 2)
+(make_list_size 2 '(2))
+(make_list_size 2 '(a b c))
